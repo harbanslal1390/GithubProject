649d8b9175275ef14d6f90af0575f350533b22f7
Admin <Admin@192.168.20.7>
2024-08-10 13:26:23 +1000

    ok

--- ConstructorwithThis/src/FirstClass.java
+++ ConstructorwithThis/src/FirstClass.java
@@ -1,7 +1,26 @@
 
 public class FirstClass {
+	public FirstClass()
+	{ this(10,20,30);
+		System.out.println("Default constructor");	
+	}
+	public FirstClass(int a)
+	{		
+		this(10,20);
+	 System.out.println("One parameter");	
+	}
+	public FirstClass(int a, int b)
+	{ this();
+		System.out.println("Two parameter");		
+	}
+	public FirstClass(int a, int b, int c)
+	{
+		System.out.println("Three parameter");	
+	}
 	public static void main(String[] args) {
-		int a;
+		
+		FirstClass obj= new FirstClass(10);
+		
 	}
 
 }
